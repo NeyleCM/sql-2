2ff2939184bb2d1a78ae698a75b3ac7568a43307
NeyleCM <neylecmorantes@gmail.com>
2024-09-18 22:50:58 +0200

    Init

--- statements.sql
+++ statements.sql
@@ -1,14 +1,28 @@
 /* Relación tipo 1:1 */
 -- PASO 1
--- Tu código aquí
+-- CREATE TABLE usuarios_2 (
+--     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+--     nombre VARCHAR(50) NOT NULL,
+--     apellido VARCHAR(100) NOT NULL,
+--     email VARCHAR(100) NOT NULL,
+--     edad INT
+-- );
 
 
 -- PASO 2
--- Tu código aquí
+-- CREATE TABLE roles (
+--     id_rol INT AUTO_INCREMENT PRIMARY KEY,
+--     nombre_rol VARCHAR(50) NOT NULL
+-- );
 
 
 -- PASO 3
--- Tu código aquí
+-- ALTER TABLE usuarios ADD FOREIGN KEY (id_rol) REFERENCES roles(id_rol);
+
+-- UPDATE usuarios_2 SET id_rol = 1 WHERE id = 1;
+-- UPDATE usuarios_2 SET id_rol = 2 WHERE id = 2;
+-- UPDATE usuarios_2 SET id_rol = 3 WHERE id = 3;
+-- UPDATE usuarios_2 SET id_rol = 4 WHERE id = 4;
 
 
 -- PASO 4
